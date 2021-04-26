aab13bb37fcbcf0cef70f9f9cf956570ecdd0fc9
Jack Baty <jack@baty.net>
2021-04-26 07:08:34 -0400

    Add date to dailies header

--- orgroam.el
+++ orgroam.el
@@ -15,4 +15,4 @@
 (setq org-roam-dailies-capture-templates
       '(("d" "default" entry "* %?" :if-new
          (file+head "%(concat org-roam-dailies-directory \"/%<%Y-%m-%d>.org\")"
-                    "#+title: %<%Y-%m-%d>"))))
+                    "#+title: Daily Notes - %<%A, %B %e %Y>\n#+date:  %<%Y-%m-%d>"))))
