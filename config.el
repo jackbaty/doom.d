ada2688bc3684d7963e5e9efd5aac09af7577fdd
Jack Baty <jack@baty.net>
2021-05-31 19:57:43 -0400

    Back to modus

--- config.el
+++ config.el
@@ -26,8 +26,8 @@
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 ;;(setq doom-theme 'doom-flatwhite)
-;;(setq doom-theme 'modus-operandi)
-(setq doom-theme 'doom-one)
+(setq doom-theme 'modus-operandi)
+;;(setq doom-theme 'doom-one)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
