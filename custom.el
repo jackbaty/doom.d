7afa7c78e5dd9bef65ada661f55134d265ab29bd
Jack Baty <jack@baty.net>
2021-08-17 20:18:08 -0400

    Custom.el

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-agenda-files
-   '("~/org/roam/20210816104936-wifi_for_ella_s_house.org" "/Users/jbaty/org/roam/20210816085624-fusionary_r_d_tax_credit.org" "/Users/jbaty/org/todo.org" "/Users/jbaty/org/notes.org" "/Users/jbaty/baty.blog/content-org/posts.org" "/Users/jbaty/org/events.org" "/Users/jbaty/org/food.org" "/Users/jbaty/org/daybook.org")))
+   '("~/org/roam/20210817084110-electrical_updates_2021.org" "/Users/jbaty/org/roam/20210816104936-wifi_for_ella_s_house.org" "/Users/jbaty/org/roam/20210816085624-fusionary_r_d_tax_credit.org" "/Users/jbaty/org/todo.org" "/Users/jbaty/baty.blog/content-org/posts.org" "/Users/jbaty/org/events.org" "/Users/jbaty/org/food.org" "/Users/jbaty/org/daybook.org")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
