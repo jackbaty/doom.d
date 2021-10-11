1ab7beb2131cee6a0524fdaca8d54f20094a2886
Jack Baty <jack@baty.net>
2021-10-11 19:49:47 -0400

    custom updated

--- custom.el
+++ custom.el
@@ -3,8 +3,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(fci-rule-color "#a3a1a1")
+ '(jdee-db-active-breakpoint-face-colors (cons "#f2f2f2" "#4271ae"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#f2f2f2" "#718c00"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#f2f2f2" "#8e908c"))
+ '(objed-cursor-color "#c82829")
  '(org-agenda-files
-   '("~/org/roam/20210916054654-new_roof_on_220_grandville.org" "/Users/jbaty/org/roam/20210816085624-fusionary_r_d_tax_credit.org" "/Users/jbaty/org/roam/20210817084110-electrical_updates_2021.org" "/Users/jbaty/org/roam/20210816104936-wifi_for_ella_s_house.org" "/Users/jbaty/org/todo.org" "/Users/jbaty/baty.blog/content-org/posts.org" "/Users/jbaty/org/events.org" "/Users/jbaty/org/food.org" "/Users/jbaty/org/daybook.org")))
+   '("/Users/jbaty/org/roam/20210817084110-electrical_updates_2021.org" "/Users/jbaty/org/roam/20210916054654-new_roof_on_220_grandville.org" "/Users/jbaty/org/roam/20210816085624-fusionary_r_d_tax_credit.org" "/Users/jbaty/org/todo.org" "/Users/jbaty/baty.blog/content-org/posts.org" "/Users/jbaty/org/events.org" "/Users/jbaty/org/food.org" "/Users/jbaty/org/daybook.org"))
+ '(rustic-ansi-faces
+   ["#ffffff" "#c82829" "#718c00" "#eab700" "#4271ae" "#c678dd" "#8abeb7" "#4d4d4c"]))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
