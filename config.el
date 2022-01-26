de0a3c23709aac45ad79324e70f4c0d550bfcc80
Jack Baty <jack@baty.net>
2022-01-26 16:23:18 -0500

    Change fonts

--- config.el
+++ config.el
@@ -19,8 +19,8 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "FiraCode Nerd Font" :size 15)
-      doom-variable-pitch-font (font-spec :family "Roboto" :size 15))
+(setq doom-font (font-spec :family "IBM Plex Mono" :size 15)
+      doom-variable-pitch-font (font-spec :family "IBM Plex Sans" :size 15))
 
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
