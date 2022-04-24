5db48c779a1ab7a774fee0383e43b64f1f406511
Jack Baty <jack@baty.net>
2022-04-24 05:25:31 -0400

    Add function to recenter frame
    From Louis Brauer https://discord.gg/QYFcqNT

--- lisp/myfunctions.el
+++ lisp/myfunctions.el
@@ -84,3 +84,13 @@ this can take a second or two to execute."
  "Run consult-ripgrep on the org roam directory"
  (interactive)
  (consult-ripgrep org-roam-directory nil))
+
+;; recenter frame
+(defun jab/frame-recenter (&optional frame)
+  "Center FRAME on the screen.
+FRAME can be a frame name, a terminal name, or a frame.
+If FRAME is omitted or nil, use currently selected frame."
+  (interactive)
+  (unless (eq 'maximised (frame-parameter nil 'fullscreen))
+    (modify-frame-parameters
+     frame '((user-position . t) (top . 0.5) (left . 0.5)))))
