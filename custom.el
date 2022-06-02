b62a71f778e5145008101a060b9b251e0d972784
Jack Baty <jack@baty.net>
2022-06-02 10:43:40 -0400

    Remove org-agenda-files from custom

--- custom.el
+++ custom.el
@@ -9,8 +9,6 @@
  '(jdee-db-requested-breakpoint-face-colors (cons "#f2f2f2" "#718c00"))
  '(jdee-db-spec-breakpoint-face-colors (cons "#f2f2f2" "#8e908c"))
  '(objed-cursor-color "#c82829")
- '(org-agenda-files
-   '("~/org/roam/projects/20220302123550-220_grandville_tenent_search_2022.org" "/Users/jbaty/org/roam/projects/20220406-kitchen_remodel_2022.org" "/Users/jbaty/org/todo.org" "/Users/jbaty/org/projects.org" "/Users/jbaty/org/notes.org" "/Users/jbaty/org/events.org" "/Users/jbaty/org/food.org" "/Users/jbaty/org/doing.org" "/Users/jbaty/org/daybook.org" "/Users/jbaty/Library/Mobile Documents/iCloud~is~workflow~my~workflows/Documents/voicenotes.org"))
  '(rustic-ansi-faces
    ["#ffffff" "#c82829" "#718c00" "#eab700" "#4271ae" "#c678dd" "#8abeb7" "#4d4d4c"])
  '(safe-local-variable-values
