fc9b96db0bc42e58e2a5461600cf4b3919bee5a4
Jack Baty <jack@baty.net>
2022-12-28 05:55:06 -0500

    Change default mailbox sort direction

--- lisp/mu4e.el
+++ lisp/mu4e.el
@@ -28,6 +28,7 @@
    +org-capture-emails-file "~/org/tasks.org"
    mu4e-headers-precise-alignment t
    mu4e-update-interval 300
+   mu4e-headers-sort-direction 'ascending
  mu4e-headers-fields
         '((:human-date . 12)
           (:flags . 4)
