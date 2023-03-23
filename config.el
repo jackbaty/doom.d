9b1079ccb8877568d92a831621caebb4cdc4dd7f
Jack Baty <jack@baty.net>
2023-03-23 06:46:20 -0400

    Font changes. Remove NANO

--- config.el
+++ config.el
@@ -21,8 +21,8 @@
 ;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
 ;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
 
-(setq doom-font (font-spec :family "Overpass Mono" :size 16)
-      doom-variable-pitch-font (font-spec :family "Overpass" :size 16))
+(setq doom-font (font-spec :family "IBM Plex Mono" :size 15)
+      doom-variable-pitch-font (font-spec :family "IBM Plex Sans" :size 15))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
@@ -35,13 +35,13 @@
 ;; See below for NANO theme
 (setq doom-theme 'doom-one)
 
-(after! doom-themes
-  (load-theme 'doom-nano-light t))
+;; (after! doom-themes
+;;   (load-theme 'doom-nano-light t))
 
-(use-package! doom-nano-modeline
-  :config
-  (doom-nano-modeline-mode 1)
-  (global-hide-mode-line-mode 1))
+;; (use-package! doom-nano-modeline
+;;   :config
+;;   (doom-nano-modeline-mode 1)
+;;   (global-hide-mode-line-mode nil))
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -87,8 +87,8 @@
 ;; **** HERE WE GO ***
 
 
-(setq initial-frame-alist '((width . 200) (height . 61)))
-(setq default-frame-alist '((width . 150) (height . 55)))
+(setq initial-frame-alist '((width . 160) (height . 55)))
+(setq default-frame-alist '((width . 160) (height . 55)))
 (add-to-list 'after-make-frame-functions #'jab/frame-center)
 
 (setq bookmark-default-file "~/Sync/emacs/bookmarks")
@@ -114,15 +114,6 @@
 (use-package! orderless
   :custom (completion-styles '(orderless)))
 
-;; Load my "modules"
-(load! "lisp/orgmode")
-;;(load! "lisp/denote")
-(load! "lisp/orgroam")
-(load! "lisp/latex")
-(load! "lisp/mappings")
-(load! "lisp/myfunctions")
-(load! "lisp/mu4e")
-;;(load! "lisp/notmuch")
 
 ;; For markdown->html exports
 (setq markdown-css-paths '("https://static.baty.net/cdn/simple.min.css"))
@@ -137,3 +128,49 @@
 (setq gc-cons-threshold (* 100 1024 1024)
       read-process-output-max (* 1024 1024))
 
+(setq emojify-emoji-styles '(unicode github))
+
+
+;; Configure Tempel
+(use-package tempel
+  ;; Require trigger prefix before template name when completing.
+  ;; :custom
+  ;; (tempel-trigger-prefix "<")
+
+  :bind (("M-+" . tempel-complete) ;; Alternative tempel-expand
+         ("M-*" . tempel-insert))
+
+  :init
+
+  ;; Setup completion at point
+  (defun tempel-setup-capf ()
+    ;; Add the Tempel Capf to `completion-at-point-functions'.
+    ;; `tempel-expand' only triggers on exact matches. Alternatively use
+    ;; `tempel-complete' if you want to see all matches, but then you
+    ;; should also configure `tempel-trigger-prefix', such that Tempel
+    ;; does not trigger too often when you don't expect it. NOTE: We add
+    ;; `tempel-expand' *before* the main programming mode Capf, such
+    ;; that it will be tried first.
+    (setq-local completion-at-point-functions
+                (cons #'tempel-expand
+                      completion-at-point-functions)))
+
+  (add-hook 'prog-mode-hook 'tempel-setup-capf)
+  (add-hook 'text-mode-hook 'tempel-setup-capf)
+  (add-hook 'org-mode-hook 'tempel-setup-capf)
+
+  ;; Optionally make the Tempel templates available to Abbrev,
+  ;; either locally or globally. `expand-abbrev' is bound to C-x '.
+  ;; (add-hook 'prog-mode-hook #'tempel-abbrev-mode)
+  ;; (global-tempel-abbrev-mode)
+  (setq tempel-path "~/.config/emacs/my-templates")
+)
+
+;; Load my "modules"
+(load! "lisp/orgmode")
+(load! "lisp/orgroam")
+(load! "lisp/latex")
+(load! "lisp/mappings")
+(load! "lisp/myfunctions")
+(load! "lisp/mu4e")
+;;(load! "lisp/notmuch")
