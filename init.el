4d63d46098975999f2feced917f81765c95ad05b
Jack Baty <jack@baty.net>
2023-03-28 07:23:10 -0400

    From notmuch back to Mu4e

--- init.el
+++ init.el
@@ -183,8 +183,8 @@
        ;;zig               ; C, but simpler
 
        :email
-       ;;mu4e
-       notmuch
+       (mu4e +org)
+       ;;(notmuch +org)
        ;;(wanderlust +gmail)
 
        :app
