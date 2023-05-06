ebcd0978af75866d12dc63bc39589fd12d9f1dc5
Jack Baty <jack@baty.net>
2023-05-06 07:33:07 -0400

    Fix fringes in modus themes

--- config.el
+++ config.el
@@ -41,7 +41,7 @@
       modus-operandi-theme-slanted-constructs t
       modus-operandi-theme-bold-constructs t
       modus-operandi-theme-fringes 'nil ; {nil,'subtle,'intense}
-      modus-theme-fringes 'nil ; {nil,'subtle,'intense}
+      modus-themes-fringes 'nil ; {nil,'subtle,'intense}
       modus-operandi-theme-3d-modeline t
       modus-operandi-theme-faint-syntax t
       modus-operandi-theme-intense-hl-line t
